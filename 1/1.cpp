6c73cbc91205318569f3640f1b5299f1751829b4
Карина Власова <vlasova.karina.alexandrovna@gmail.com>
2020-12-04 16:36:49 +0300

    Добавьте файлы проекта.

---
+++ 1/1.cpp
@@ -0,0 +1,28 @@
+﻿using namespace std;
+#include <iostream>
+#include <string>
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int n = 1;
+	char a[n] = { 'a' };
+	const int l = 15;
+	char b[l] = "KarinaVlasova";
+	cout << "Символ - a\n";
+	cout << "Строка - KarinaVlasova\n";
+	int k = 0;
+	for (int i = 0; b[i]; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (b[i] == a[j])
+				k++;
+		}
+	}
+	if (k)
+		cout << "Количество найденных символов: " << k << endl;
+	else
+		cout << "Данного символа не найдено!";
+	cin.get();
+	return 0;
+}
---
+++ 4/4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	const int N = 14;
+	char vowel[N] = { 'A', 'a', 'E', 'e', 'I', 'i', 'J', 'j', 'O', 'o', 'U', 'u', 'Y', 'y' };
+	string str;
+	int k;
+	cout << "Введите строку из латинских букв):" << endl;
+	getline(cin, str);
+	k = 0;
+	for (int i = 0; str[i]; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (str[i] == vowel[j])
+				k++;
+		}
+	}
+	if (k)
+		cout << "Количество гласных символов: " << k << endl;
+	else
+		cout << "Гласных символов не найдено" << endl;
+	cin.get();
+	return 0;
+}

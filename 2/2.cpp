a326ac62504a95c48a4154c6d1dac22b6208b914
Карина Власова <vlasova.karina.alexandrovna@gmail.com>
2020-12-11 12:28:58 +0300

    .

--- 2/2.cpp
+++ 2/2.cpp
@@ -1,1 +1,20 @@
-﻿
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	cout<< "Введите строку на английском языке"<< endl;
+	string stroka;
+	cin>> stroka;
+	cout<< "Введите символ, который необходимо заменить"<< endl;
+	string simvol;
+	cin >> simvol;
+	cout << "Введите символ, на который хотите заменить" << endl;
+	string simvolz;
+	cin >> simvolz;
+	string::size_type index;
+	while ((index = stroka.find(simvol)) != string::npos)
+		stroka.replace(index, simvol.size(), simvolz);
+	cout << stroka << endl;
+	return 0;
+}
